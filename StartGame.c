66a0ade090cb667114b2db83e3521eb477e7b2ef
Vaskka <32007921+Vaskka@users.noreply.github.com>
2017-12-02 18:03:48 +0800

    游戏客户端

---
+++ StartGame.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+	char* start = "python TankWar.py";
+	puts("=================================");
+	puts("=                               =");
+	puts("=                               =");
+	puts("=        欢迎来到坦克大战       =");
+	puts("=        ----developed by Vaskka=");
+	puts("=                               =");
+	puts("=                               =");
+	puts("=================================");
+	puts("\n");
+	system("pause");
+	system(start);
+	system("pause");
+	return 0;
+}
